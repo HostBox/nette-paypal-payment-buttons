d3fb9b25261642a7d4c3f789240c444830d96ce6
EIFEL <czeifel@gmail.com>
2013-12-26 22:38:35 +0100

    Add logoUrl and custom button field

--- src/HostBox/Components/PayPal/PaymentButtons/BasicButton.php
+++ src/HostBox/Components/PayPal/PaymentButtons/BasicButton.php
@@ -8,12 +8,21 @@ use Nette\Application as Nette;
 abstract class BasicButton extends PaypalPaymentButton {
 
     /** @var int */
-    public $quantity;
+    public $quantity = 1;
 
     /** @var float */
     public $shipping;
 
     /** @var float */
     public $tax;
+
+    /**
+     * @var string
+     * @name cpp_logo_image
+     */
+    public $logoUrl;
+
+    /** @var mixed */
+    public $custom;
 
 }
